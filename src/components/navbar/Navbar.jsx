e00e4ae68ba72d78b61c03f26f9124aceabdc71e
Talal2003 <www.talal2003@gmail.com>
2025-12-10 16:29:59 -0500

    initial structure for navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,34 @@
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import { Link } from '@mui/material';
+import { Link as RouterLink } from 'react-router-dom';
+
 export default function Navbar() {
   return (
-    <div>
-      Navbar
-    </div>
-  )
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Tech Heim
+          </Typography>
+
+          <Box sx={{ display:'flex', gap: 4}}>
+            <Link component={RouterLink} to='/home' color='inherit' underline='none'>Home</Link>
+            <Link component={RouterLink} to='/cart' color='inherit' underline='none'>Products</Link>
+            <Link component={RouterLink} to='/home' color='inherit' underline='none'>Blog</Link>
+            <Link component={RouterLink} to='/home' color='inherit' underline='none'>FAQ</Link>
+            <Link component={RouterLink} to='/home' color='inherit' underline='none'>Contact Us</Link>
+            <Link component={RouterLink} to='/auth/login' color='inherit' underline='none'>Login</Link>
+            <Link component={RouterLink} to='/auth/register' color='inherit' underline='none'>Register</Link>
+          </Box>
+
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import './index.css'
+import { CssBaseline } from '@mui/material';
 
-createRoot(document.getElementById('root')).render(
-
+ReactDOM.createRoot(document.getElementById('root')).render(
+<>
+<CssBaseline />
     <App />
+</>
 
 )
