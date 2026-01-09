16279ff9b936dd77727f4678284c22b923403c8a
Talal2003 <www.talal2003@gmail.com>
2026-01-08 19:08:12 -0500

    integrate cart data hook and render dynamic items

--- src/hooks/useAddToCart.js
+++ src/hooks/useAddToCart.js
@@ -1,14 +1,16 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import axiosAuthInstance from "../api/axiosAuthInstance";
 
 export default function useAddToCart() {
-
+    const queryClient = useQueryClient();
     const addToCartMutation = useMutation({
         mutationFn: async ({ ProductId, Count }) => {
             return await axiosAuthInstance.post('/Carts', {
                 ProductId,
                 Count
             })
+        }, onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ['carts'] });
         }
     })
 
--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Card, CardActions, CardContent, CardMedia, Divider, Grid, IconButton, Typography } from "@mui/material";
+import { Box, Button, Card, CardActions, CardContent, CardMedia, CircularProgress, Divider, Grid, IconButton, Typography } from "@mui/material";
 import InventoryIcon from '@mui/icons-material/Inventory';
 import VerifiedIcon from '@mui/icons-material/Verified';
 import LocalShippingIcon from '@mui/icons-material/LocalShipping';
@@ -8,161 +8,68 @@ import useCart from "../../hooks/useCart";
 
 export default function Cart() {
 
-const {data} = useCart();
-console.log(data);
+  const { data, isLoading, isError } = useCart();
+  if (isLoading) return <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', py: 3 }}>
+    <CircularProgress />
+  </Box>
+  if (isError) return <Typography sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', py: 3, color: 'red' }}>
+    {error.message}
+  </Typography>
 
   return (
     <>
       <Grid container spacing={{ xs: 3, md: 6, lg: 13 }}>
         <Grid size={{ xs: 12, md: 7, lg: 8 }}>
           <Box sx={{ display: "flex", flexDirection: "column", gap: 3 }}>
-            <Card sx={{ display: "flex", flexDirection: "row", gap: 3, px: 2, py: 1, alignItems: "center" }}>
-              <CardMedia
-                component="img"
-                sx={{ width: 182, height: '100%', borderRadius: 2 }}
-                image="https://mui.com/static/images/cards/contemplative-reptile.jpg"
-                title=""
-              />
-              <CardContent sx={{ display: "flex", flexDirection: "column", gap: 3, flex: 1 }}>
-                <Typography>MacBook Pro M2 MNEJ3 2022 LLA 13.3 inch</Typography>
-                <Box sx={{ display: "flex", flexDirection: "column", gap: 1 }}>
-                  <Box display="flex" flexDirection="row" gap={0.5} alignItems={'center'}>
-                    <InventoryIcon fontSize="small" />
-                    <Typography variant="body2">In Stock</Typography>
-                  </Box>
-                  <Box display="flex" flexDirection="row" gap={0.5} alignItems={'center'}>
-                    <VerifiedIcon fontSize="small" />
-                    <Typography variant="body2">Guaranteed</Typography>
-                  </Box>
-                  <Box display="flex" flexDirection="row" gap={0.5} alignItems={'center'}>
-                    <LocalShippingIcon fontSize="small" />
-                    <Typography variant="body2">Free Delivery</Typography>
-                  </Box>
-                </Box>
-                <Box sx={{ display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
-                  <Box sx={{ display: "flex", flexDirection: "row", gap: 1, alignItems: "center" }}>
-                    <Typography variant="subtitle1">
-                      $1299.00
-                    </Typography>
-                  </Box>
-                  <Box sx={{ display: "flex", flexDirection: "row", gap: 1 }}>
-                    <IconButton sx={{ mt: "auto", p: 0 }}>
-                      <DeleteOutlineIcon fontSize="small" sx={{ color: "red" }} />
-                    </IconButton>
-                    <Box sx={{
-                      display: "flex", flexDirection: "row", gap: 1.5, alignItems: "center",
-                      width: "100%", borderBottom: "1px solid #717171"
-                    }}>
-                      <Button sx={{ px: 0.4375, py: 0, minWidth: 0 }}>
-                        <Typography variant="h4" sx={{ color: "#717171" }}>-</Typography>
-                      </Button>
-                      <Typography variant="h6" sx={{ color: "#717171" }}>1</Typography>
-                      <Button sx={{ px: 0.4375, py: 0, minWidth: 0 }}>
-                        <Typography variant="h5" sx={{ color: "#717171" }}>+</Typography>
-                      </Button>
+            {data.items.map(item => (
+              <Card key={item.productId} sx={{ display: "flex", flexDirection: "row", gap: 3, px: 2, py: 1, alignItems: "center" }}>
+                <CardMedia
+                  component="img"
+                  sx={{ width: 182, height: '100%', borderRadius: 2 }}
+                  image="https://mui.com/static/images/cards/contemplative-reptile.jpg"
+                  title=""
+                />
+                <CardContent sx={{ display: "flex", flexDirection: "column", gap: 3, flex: 1 }}>
+                  <Typography>{item.productName}</Typography>
+                  <Box sx={{ display: "flex", flexDirection: "column", gap: 1 }}>
+                    <Box display="flex" flexDirection="row" gap={0.5} alignItems={'center'}>
+                      <InventoryIcon fontSize="small" />
+                      <Typography variant="body2">In Stock</Typography>
+                    </Box>
+                    <Box display="flex" flexDirection="row" gap={0.5} alignItems={'center'}>
+                      <VerifiedIcon fontSize="small" />
+                      <Typography variant="body2">Guaranteed</Typography>
+                    </Box>
+                    <Box display="flex" flexDirection="row" gap={0.5} alignItems={'center'}>
+                      <LocalShippingIcon fontSize="small" />
+                      <Typography variant="body2">Free Delivery</Typography>
                     </Box>
                   </Box>
-                </Box>
-              </CardContent>
-            </Card>
-            <Card sx={{ display: "flex", flexDirection: "row", gap: 3, px: 2, py: 1, alignItems: "center" }}>
-              <CardMedia
-                component="img"
-                sx={{ width: 182, height: '100%', borderRadius: 2 }}
-                image="https://mui.com/static/images/cards/contemplative-reptile.jpg"
-                title=""
-              />
-              <CardContent sx={{ display: "flex", flexDirection: "column", gap: 3, flex: 1 }}>
-                <Typography>MacBook Pro M2 MNEJ3 2022 LLA 13.3 inch</Typography>
-                <Box sx={{ display: "flex", flexDirection: "column", gap: 1 }}>
-                  <Box display="flex" flexDirection="row" gap={0.5} alignItems={'center'}>
-                    <InventoryIcon fontSize="small" />
-                    <Typography variant="body2">In Stock</Typography>
-                  </Box>
-                  <Box display="flex" flexDirection="row" gap={0.5} alignItems={'center'}>
-                    <VerifiedIcon fontSize="small" />
-                    <Typography variant="body2">Guaranteed</Typography>
-                  </Box>
-                  <Box display="flex" flexDirection="row" gap={0.5} alignItems={'center'}>
-                    <LocalShippingIcon fontSize="small" />
-                    <Typography variant="body2">Free Delivery</Typography>
-                  </Box>
-                </Box>
-                <Box sx={{ display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
-                  <Box sx={{ display: "flex", flexDirection: "row", gap: 1, alignItems: "center" }}>
-                    <Typography variant="subtitle1">
-                      $1299.00
-                    </Typography>
-                  </Box>
-                  <Box sx={{ display: "flex", flexDirection: "row", gap: 1 }}>
-                    <IconButton sx={{ mt: "auto", p: 0 }}>
-                      <DeleteOutlineIcon fontSize="small" sx={{ color: "red" }} />
-                    </IconButton>
-                    <Box sx={{
-                      display: "flex", flexDirection: "row", gap: 1.5, alignItems: "center",
-                      width: "100%", borderBottom: "1px solid #717171"
-                    }}>
-                      <Button sx={{ px: 0.4375, py: 0, minWidth: 0 }}>
-                        <Typography variant="h4" sx={{ color: "#717171" }}>-</Typography>
-                      </Button>
-                      <Typography variant="h6" sx={{ color: "#717171" }}>1</Typography>
-                      <Button sx={{ px: 0.4375, py: 0, minWidth: 0 }}>
-                        <Typography variant="h5" sx={{ color: "#717171" }}>+</Typography>
-                      </Button>
+                  <Box sx={{ display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
+                    <Box sx={{ display: "flex", flexDirection: "row", gap: 1, alignItems: "center" }}>
+                      <Typography variant="subtitle1">${item.price}</Typography>
+                    </Box>
+                    <Box sx={{ display: "flex", flexDirection: "row", gap: 1 }}>
+                      <IconButton sx={{ mt: "auto", p: 0 }}>
+                        <DeleteOutlineIcon fontSize="small" sx={{ color: "red" }} />
+                      </IconButton>
+                      <Box sx={{
+                        display: "flex", flexDirection: "row", gap: 1.5, alignItems: "center",
+                        width: "100%", borderBottom: "1px solid #717171"
+                      }}>
+                        <Button sx={{ px: 0.4375, py: 0, minWidth: 0 }}>
+                          <Typography variant="h4" sx={{ color: "#717171" }}>-</Typography>
+                        </Button>
+                        <Typography variant="h6" sx={{ color: "#717171" }}>{item.count}</Typography>
+                        <Button sx={{ px: 0.4375, py: 0, minWidth: 0 }}>
+                          <Typography variant="h5" sx={{ color: "#717171" }}>+</Typography>
+                        </Button>
+                      </Box>
                     </Box>
                   </Box>
-                </Box>
-              </CardContent>
-            </Card>
-            <Card sx={{ display: "flex", flexDirection: "row", gap: 3, px: 2, py: 1, alignItems: "center" }}>
-              <CardMedia
-                component="img"
-                sx={{ width: 182, height: '100%', borderRadius: 2 }}
-                image="https://mui.com/static/images/cards/contemplative-reptile.jpg"
-                title=""
-              />
-              <CardContent sx={{ display: "flex", flexDirection: "column", gap: 3, flex: 1 }}>
-                <Typography>MacBook Pro M2 MNEJ3 2022 LLA 13.3 inch</Typography>
-                <Box sx={{ display: "flex", flexDirection: "column", gap: 1 }}>
-                  <Box display="flex" flexDirection="row" gap={0.5} alignItems={'center'}>
-                    <InventoryIcon fontSize="small" />
-                    <Typography variant="body2">In Stock</Typography>
-                  </Box>
-                  <Box display="flex" flexDirection="row" gap={0.5} alignItems={'center'}>
-                    <VerifiedIcon fontSize="small" />
-                    <Typography variant="body2">Guaranteed</Typography>
-                  </Box>
-                  <Box display="flex" flexDirection="row" gap={0.5} alignItems={'center'}>
-                    <LocalShippingIcon fontSize="small" />
-                    <Typography variant="body2">Free Delivery</Typography>
-                  </Box>
-                </Box>
-                <Box sx={{ display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
-                  <Box sx={{ display: "flex", flexDirection: "row", gap: 1, alignItems: "center" }}>
-                    <Typography variant="subtitle1">
-                      $1299.00
-                    </Typography>
-                  </Box>
-                  <Box sx={{ display: "flex", flexDirection: "row", gap: 1 }}>
-                    <IconButton sx={{ mt: "auto", p: 0 }}>
-                      <DeleteOutlineIcon fontSize="small" sx={{ color: "red" }} />
-                    </IconButton>
-                    <Box sx={{
-                      display: "flex", flexDirection: "row", gap: 1.5, alignItems: "center",
-                      width: "100%", borderBottom: "1px solid #717171"
-                    }}>
-                      <Button sx={{ px: 0.4375, py: 0, minWidth: 0 }}>
-                        <Typography variant="h4" sx={{ color: "#717171" }}>-</Typography>
-                      </Button>
-                      <Typography variant="h6" sx={{ color: "#717171" }}>1</Typography>
-                      <Button sx={{ px: 0.4375, py: 0, minWidth: 0 }}>
-                        <Typography variant="h5" sx={{ color: "#717171" }}>+</Typography>
-                      </Button>
-                    </Box>
-                  </Box>
-                </Box>
-              </CardContent>
-            </Card>
+                </CardContent>
+              </Card>
+            ))}
           </Box>
         </Grid>
         <Grid size={{ xs: 12, md: 5, lg: 4 }}>
@@ -172,23 +79,17 @@ console.log(data);
               <Typography component={'h4'} variant="h5">Payment Details</Typography>
               <Box sx={{ display: "flex", flexDirection: "column", gap: 1.5, p: 1 }}>
                 <Box sx={{ display: "flex", flexDirection: "column", gap: 1 }}>
-                  <Box sx={{ display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
-                    <Typography variant="body2" sx={{ color: "#717171" }}>Subtotal</Typography>
-                    <Typography variant="body2" sx={{ color: "#717171" }}>$519.52</Typography>
-                  </Box>
-                  <Box sx={{ display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
-                    <Typography variant="body2" sx={{ color: "#717171" }}>Discount</Typography>
-                    <Typography variant="body2" sx={{ color: "#717171" }}>-$111.87</Typography>
-                  </Box>
-                  <Box sx={{ display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
-                    <Typography variant="body2" sx={{ color: "#717171" }}>Shipment cost</Typography>
-                    <Typography variant="body2" sx={{ color: "#717171" }}>$22.50</Typography>
-                  </Box>
+                  {data.items.map(item => (
+                    <Box sx={{ display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
+                      <Typography variant="body2" sx={{ color: "#717171" }}>{item.productName}</Typography>
+                      <Typography variant="body2" sx={{ color: "#717171" }}>${item.totalPrice}</Typography>
+                    </Box>
+                  ))}
                 </Box>
                 <Divider sx={{ borderBottomWidth: '2px' }}></Divider>
                 <Box sx={{ display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
                   <Typography component={'h6'} variant="body1" sx={{ color: "#2D2D2D" }}>Grand Total</Typography>
-                  <Typography component={'h6'} variant="body1" sx={{ color: "#2D2D2D" }}>$543.02</Typography>
+                  <Typography component={'h6'} variant="body1" sx={{ color: "#2D2D2D" }}>${data.cartTotal}</Typography>
                 </Box>
               </Box>
             </CardContent>
